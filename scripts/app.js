b548bf28b728d665cdcd35472242a7962f543d54
amansgz <amansgz.ar@gmail.com>
2023-05-22 17:44:52 -0300

    Add styles and fetch json to show data in html

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+const url = './scripts/data.json';
+
+fetch(url)
+.then(response => response.json())
+.then(data => showInfo(data));
+
+function showInfo(data) {
+  let output = '';
+  for(let item of data) {
+    output += `
+    <div class= 'score'>
+      <div>
+        <img class= 'score-icon'
+        src= ${item.icon} alt= 'icon category'>
+        <p class= 'score-category'>${item.category}</p>
+      </div>
+      <div>
+        <span class= 'score-bold'>${item.score} </span>
+        <span class= 'score-light'> / 100</span>
+      </div>  
+    </div>  
+    `
+  }
+  document.getElementById('scores').innerHTML = output;
+}
